6bedd1dc7874f253d7e564bb86dfca3546a00e6f
Santerson <santacap2019@gmail.com>
2026-03-16 13:59:44 -0700

    noijfjni

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -1,13 +1,26 @@
+/********************************************
+ * filename: GameSceneManager.cs
+ * Author: Santiago Caprarulo
+ * Description: Contains functions that can be called from gameobjects
+ * to change scenes in runtime
+ * ******************************************/
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
+    /// <summary>
+    /// Loads the game scene (or scene named "GameScene")
+    /// </summary>
     public void LoadGameScene()
     {
         SceneManager.LoadScene("GameScene");
     }
 
+    /// <summary>
+    /// Loads the main menu (or scene named "MainMenu")
+    /// </summary>
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
