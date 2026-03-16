a65a76a9ea4384f4d1fe06593197d295772d4b7c
Santerson <santacap2019@gmail.com>
2026-03-16 14:57:19 -0700

    HEHEHE I DONT KNOW IF THIS CODE WORKS EEEEEEE

--- Assets/Scripts/TowerGrid.cs
+++ Assets/Scripts/TowerGrid.cs
@@ -34,15 +34,21 @@ public class TowerGrid : MonoBehaviour
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Try to place a tower at a given position
+    /// </summary>
+    /// <param name="tower">A PREFAB of the object you are trying to instantiatew</param>
+    /// <param name="position">A position to place the tower in gridscale, in row col</param>
+    /// <return>true if the tower was successfully placed, false otherwise</return>"
+    public bool DropTower(GameObject tower, Vector2Int position)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Check if there is a open slot (SpaceStatus = unused) at this point
+        SpaceStatus refSpace = spaceStatuses[position.x][position.y];
+            // If so, add it to the grid and update all arrays with it
+            // Instantiate a game object there
+            // Save a reference to that object in the third 2d array
+            // return true
+        // otherwise, return false
+        return false;
     }
 }
