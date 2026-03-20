90f44b291b8e3b44b255dbdcd4c83cb236da1d43
mictheprofit <micaiah.mariano@digipen.edu>
2026-03-20 13:15:59 -0700

    stuff

--- Assets/Scripts/EnemyStuff/EnemyBoxAi.cs
+++ Assets/Scripts/EnemyStuff/EnemyBoxAi.cs
@@ -1,20 +1,29 @@
+/********************************************
+ * filename: EnemyBoxAi.cs
+ * Author: Micaiah Mariano
+ * Description: Contains the logic for the attack box of enemys
+ * ******************************************/
 using UnityEngine;
+
 
 public class EnemyBoxAi : MonoBehaviour
 {
+    // determines size
     [SerializeField] float SizeX = 1;
     [SerializeField] float SizeY = 1;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //sets size
         Vector3 Size = new Vector3(SizeX, SizeY);
         gameObject.transform.localScale = Size;
     }
 
-    // Update is called once per frame
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //checks for collision with tower that destroy itself if it did
         if (collision.CompareTag("Tower"))
         {
             Destroy(gameObject);
