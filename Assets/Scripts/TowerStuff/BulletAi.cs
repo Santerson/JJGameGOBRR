f883485cec53555cd6d81328f674ce22a1e79786
mictheprofit <micaiah.mariano@digipen.edu>
2026-03-20 12:51:42 -0700

    comments

--- Assets/Scripts/TowerStuff/BulletAi.cs
+++ Assets/Scripts/TowerStuff/BulletAi.cs
@@ -1,28 +1,38 @@
+/********************************************
+ * filename: BulletAi.cs
+ * Author: Micaiah Mariano
+ * Description: Contains the logic for the bullets form towers
+ * ******************************************/
 using UnityEngine;
 
 public class BulletAi : MonoBehaviour
 {
+    // size and color
     [SerializeField] float SizeX = 1;
     [SerializeField] float SizeY = 1;
     [SerializeField] Color refcolor = Color.white;
+    //stats
     [SerializeField] float Speed = 10;
+
     private Rigidbody2D RB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // initializes variables such as size and color
         RB = GetComponent<Rigidbody2D>();
         Vector3 Size = new Vector3(SizeX, SizeY);
         gameObject.transform.localScale = Size;
         gameObject.GetComponentInChildren<SpriteRenderer>().color = refcolor;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        // moves the bullet
         RB.linearVelocityX = Speed;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //checks for collision with enemy then dies
         if (collision.CompareTag("enemy"))
         {
             Destroy(gameObject);
