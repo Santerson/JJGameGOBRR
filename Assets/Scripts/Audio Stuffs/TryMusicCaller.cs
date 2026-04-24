4fc97eb26a23a22ae4d4629379e87eadf022f258
Santerson <santacap2019@gmail.com>
2026-04-24 12:56:34 -0700

    COMMENTS

--- Assets/Scripts/Audio Stuffs/TryMusicCaller.cs
+++ Assets/Scripts/Audio Stuffs/TryMusicCaller.cs
@@ -1,3 +1,10 @@
+/***********************************
+ * Filename: TryMusicCaller
+ * Author: Santiago Caprarulo
+ * Description: Contains functions to try and call certain sound effects.
+ * This is due to AudioManager only existing on the main menu
+ * ********************************/
+
 using UnityEngine;
 using static AudioManager;
 
@@ -12,30 +19,46 @@ public class TryCallAudioEfxs : MonoBehaviour
             Debug.LogWarning("No Audiomanager found. If the main menu has not occured, this is okay! No Music will play!");
     }
     
+    /// <summary>
+    /// Tries to play the main menu music
+    /// </summary>
     public void TryCallMainMenu()
     {
         if (audioManager == null) return;
         audioManager.PlayMenuMusic();
     }
 
+    /// <summary>
+    /// Tries to play the lose level music
+    /// </summary>
     public void TryCallLose()
     {
         if (audioManager == null) return;
         audioManager.PlayLoseLevelMusic();
     }
 
+    /// <summary>
+    /// Tries to play the win level music
+    /// </summary>
     public void TryCallWin()
     {
         if (audioManager == null) return;
         audioManager.PlayWinLevelMusic();
     }
 
+    /// <summary>
+    /// Tries to call the battle music
+    /// </summary>
+    /// <param name="type">The intensity of the battle</param>
     public void TryCallBattle(int type = 1)
     {
         if (audioManager == null) return;
         audioManager.PlayGameMusic(type);
     }
 
+    /// <summary>
+    /// Tries to call the button click sfx
+    /// </summary>
     public void TryCallButtonClickSFX()
     {
         if (audioManager == null) return;
--- Assets/Scripts/Cheats/TimeWarp.cs
+++ Assets/Scripts/Cheats/TimeWarp.cs
@@ -1,3 +1,8 @@
+/***********************************
+ * Filename: TimeWarp
+ * Author: Santiago Caprarulo
+ * Description: Contains a cheat to speed up gametime by holding down x, z, and c
+ * ********************************/
 using UnityEngine;
 
 public class TimeWarp : MonoBehaviour
@@ -13,12 +18,15 @@ public class TimeWarp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Do nothing if game is paused
         if (Time.timeScale == 0)
             return;
+        // Check for input and speed up time if so
         if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.X) && Input.GetKey(KeyCode.C))
         {
             Time.timeScale = TimeWarpedGameSpeed;
         }
+        // Otherwise, do nothing
         else
         {
             if (Time.timeScale != baseGameSpeed && Time.timeScale != TimeWarpedGameSpeed)
